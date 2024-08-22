a3ea885981b9d2b5eab2665d2feac15c498fffcc
Yair <ballinasyair@gmail.com>
2024-08-21 20:03:22 -0500

    implemented computerChoice function that gets a a random choice from paper, rock or scissors for the computer.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello, World")
+function getComputerChoice() {
+
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        return "rock";
+    } else if (computerChoice == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+    
+
+
+console.log(getComputerChoice())
