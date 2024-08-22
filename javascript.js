46049f7335ee1fce213e30972d3f1c6c3dee3abe
Yair <ballinasyair@gmail.com>
2024-08-22 13:49:13 -0500

    added playRound() function as well as playGame() function where the game is played 5 times and the score is kept track of.

--- javascript.js
+++ javascript.js
@@ -10,14 +10,59 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let userChoice = prompt("Would you like to be rock, paper or scissors?");
+    let userChoice = prompt("Would you like to be rock, paper or scissors?").toLowerCase();
     if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
         return userChoice;
     } else {
-        alert("You did not choose a correct option!")
+        alert("You did not choose a correct option. Please try again!")
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") { 
+        if (computerChoice == "rock") {
+            console.log("Its a tie! You both chose rock!");
+        } else if (computerChoice == "paper") {
+            computerScore += 1;
+            console.log("You lose. Computer chose paper!");
+        } else {
+            humanScore += 1;
+            console.log("You win!");
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            humanScore += 1;
+            console.log("You win!");
+        } else if (computerChoice == "paper") {
+            console.log("Its a tie! You both chose paper!");
+        } else {
+            computerScore += 1;
+            console.log("You lose. Computer chose scissors!");
+        }
+    } else {    // human chooses scissors
+        if (computerChoice == "rock") { 
+            computerScore += 1;
+            console.log("You lose. Computer chose rock!");
+        } else if (computerChoice == "paper") {
+            humanScore += 1;
+            console.log("You win!");
+        } else {
+            console.log("Its a tie! You both choose scissors!");
+        }
     }
 }
 
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log("Your score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+console.log(playGame());
