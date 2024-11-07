bc2881f73660abbadfa6736b26ee4f15c22ba21f
Yair <ballinasyair@gmail.com>
2024-11-06 21:32:16 -0600

    rearranged functions in file

--- javascript.js
+++ javascript.js
@@ -1,24 +1,3 @@
-function uiScoreBoard() {
-    // Rock button
-    const rockButton = document.querySelector("#rockButton");
-    rockButton.addEventListener("click", () => {
-        playRound("rock", getComputerChoice());
-    });
-
-    const paperButton = document.querySelector("#paperButton");
-    paperButton.addEventListener("click", () => {
-        playRound("paper", getComputerChoice());
-    });
-
-    const scissorsButton = document.querySelector("#scissorsButton");
-    scissorsButton.addEventListener("click", () => {
-        playRound("scissors", getComputerChoice());
-    });
-}
-function playGame() {
-    uiScoreBoard();
-}
-
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -29,15 +8,6 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-// function getHumanChoice() {
-//     let userChoice = prompt("Would you like to be rock, paper or scissors?").toLowerCase();
-//     if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
-//         return userChoice;
-//     } else {
-//         alert("You did not choose a correct option. Please try again!")
-//     }
-// }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "rock") { 
@@ -72,6 +42,33 @@ function playRound(humanChoice, computerChoice) {
         }
     }
 }
+function uiScoreBoard() {
+    // Rock button
+    const rockButton = document.querySelector("#rockButton");
+    rockButton.addEventListener("click", () => {
+        playRound("rock", getComputerChoice());
+    });
+
+    const paperButton = document.querySelector("#paperButton");
+    paperButton.addEventListener("click", () => {
+        playRound("paper", getComputerChoice());
+    });
+
+    const scissorsButton = document.querySelector("#scissorsButton");
+    scissorsButton.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice());
+    });
+}
+
+// function getHumanChoice() {
+//     let userChoice = prompt("Would you like to be rock, paper or scissors?").toLowerCase();
+//     if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+//         return userChoice;
+//     } else {
+//         alert("You did not choose a correct option. Please try again!")
+//     }
+// }
+
 
 // function playGame() {
 //     for (let i = 0; i < 5; i++) {
@@ -90,6 +87,9 @@ function playRound(humanChoice, computerChoice) {
 // gameScore.textContent = "Testing TEXT";
 // main.appendChild(gameScore);
 
+function playGame() {
+    uiScoreBoard();
+}
 
 let humanScore = 0;
 let computerScore = 0;
